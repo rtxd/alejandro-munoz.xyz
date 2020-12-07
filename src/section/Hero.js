09ed842d26bf88824d9ff7c7dbdbd71307c4709e
Alex <alejandro.munoz@student.uts.edu.au>
2020-12-07 17:30:41 +1100

    Basic hero section developed

---
+++ src/section/Hero.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 42px;
+  color: #ffffff;
+  margin-bottom: 16px;
+`;
+
+const Lede = styled.h2`
+  font-size: 24px;
+  text-transform: uppercase;
+  color: #ffffff;
+`;
+
+const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #77a6f7;
+  min-height: 80vh;
+`;
+
+function Hero() {
+  return (
+    <HeroContainer>
+      <Title>Alejandro Munoz</Title>
+      <Lede>Front-end Developer.</Lede>
+    </HeroContainer>
+  );
+}
+
+export default Hero;
