b7e22aef24d4105391c42603e185a1c9310f93d3
Alex <alejandro.munoz@student.uts.edu.au>
2021-02-09 15:02:54 +1100

    Added skills section to next js

--- components/Skills.js
+++ components/Skills.js
@@ -1,24 +1,54 @@
 import styled from "styled-components";
+import { Grid, Icon } from "semantic-ui-react";
+import styles from "../styles/Skills.module.css";
 
-const SkillsContainer = styled.section`
+const Container = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  background-color: greenyellow;
+  justify-content: start;
+  align-items: center;
+  background-color: white;
   min-height: 80vh;
 `;
 
-const Title = styled.h2`
-  font-size: 24px;
+const Title = styled.h1`
+  font-size: 42px;
   text-transform: uppercase;
-  color: #ffffff;
+  color: #00887a;
 `;
 
 export default function Skills() {
   return (
-    <SkillsContainer>
-      <Title>Skills</Title>
-    </SkillsContainer>
+    <Container>
+      <Grid verticalAlign="middle">
+        <Grid.Column floated="left">
+          <Title>Skills</Title>
+        </Grid.Column>
+        <Grid.Column floated="right">
+          <Icon name="pencil alternate" size="large"></Icon>
+        </Grid.Column>
+      </Grid>
+      <Grid className={styles.skillsSection} rows={2}>
+        <div>
+          <h3 className={styles.heading}>Languages</h3>
+          <ul>
+            <li>Javscript</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>C#</li>
+            <li>C++</li>
+            <li>Java</li>
+            <li>SQL</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className={styles.heading}>Libraries</h2>
+          <ul>
+            <li>React</li>
+            <li>Vue.js</li>
+          </ul>
+        </div>
+      </Grid>
+    </Container>
   );
 }
