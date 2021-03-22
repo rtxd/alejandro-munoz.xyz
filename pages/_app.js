09bdb333f2947fda4e0ad2a1f3881c8b030d13f9
Alex <alejandro.munoz@student.uts.edu.au>
2021-03-22 11:23:02 +1100

    reverted accidental changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 import "../styles/globals.css";
-import { theme } from "../theme/theme";
-import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ theme/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1f2833",
+    },
+    secondary: {
+      main: "#0f0",
+    },
+  },
+});
